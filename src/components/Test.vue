92ee912ce4c10049bdd71f30eb51436f47c6c057
xiaosongread <598349146@qq.com>
2019-10-22 16:29:56 +0800

    fix

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -11,36 +11,36 @@ export default {
     }
   },
   created () {
-    new Promise((resolve, reject) => {
-      console.log('log: 外部promise-1')
-      resolve()
-    })
-      .then(() => {
-        console.log('log: 外部第一个then-2')
-        new Promise((resolve, reject) => {
-          console.log('log: 内部promise-3')
-          resolve()
-        })
-          .then(() => {
-            console.log('log: 内部第一个then-4')
-            new Promise((resolve, reject) => {
-              resolve()
-            }).then(() => { console.log('log: 内部第二个then-6') })
-          })
-          .then(() => {
-            // console.log('log: 内部第二个then-6')
-          })
-      })
-      .then(() => {
-        console.log('log: 外部第二个then-5')
-      })
+    // new Promise((resolve, reject) => {
+    //   console.log('log: 外部promise-1')
+    //   resolve()
+    // })
+    //   .then(() => {
+    //     console.log('log: 外部第一个then-2')
+    //     new Promise((resolve, reject) => {
+    //       console.log('log: 内部promise-3')
+    //       resolve()
+    //     })
+    //       .then(() => {
+    //         console.log('log: 内部第一个then-4')
+    //         new Promise((resolve, reject) => {
+    //           resolve()
+    //         }).then(() => { console.log('log: 内部第二个then-6') })
+    //       })
+    //       .then(() => {
+    //         // console.log('log: 内部第二个then-6')
+    //       })
+    //   })
+    //   .then(() => {
+    //     console.log('log: 外部第二个then-5')
+    //   })
 
-      // log: 外部promise
-      // log: 外部第一个then
-      // log: 内部promise
-      // log: 内部第一个then
-      // log: 外部第二个then
-      // log: 内部第二个then
+    // log: 外部promise
+    // log: 外部第一个then
+    // log: 内部promise
+    // log: 内部第一个then
+    // log: 外部第二个then
+    // log: 内部第二个then
 
     // let p = new Promise((resolve, reject) => {
     //   setTimeout(resolve, 1000)

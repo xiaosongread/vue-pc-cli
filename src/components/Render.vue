3dbd3347a8b831a4c95f0e8cff2ab97b0fe19690
xiaosongread <598349146@qq.com>
2019-10-23 16:54:47 +0800

    render function

---
+++ src/components/Render.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'Render',
+  functional: true,
+  props: {
+    mes: ''
+  },
+  render (h, context) {
+    console.log('render', context)
+    const {mes} = context.props
+    const vnodes = []
+    vnodes.push(<div>{mes}</div>)
+    return vnodes
+  }
+}
+</script>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,59 +1,21 @@
 <template>
-  <div>123</div>
+  <div>
+    <Render :mes="mesg" ></Render>
+  </div>
 </template>
 
 <script>
+import Render from './Render'
 export default {
   name: 'Test',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      mesg: 'mesg!'
     }
   },
+  components: { Render },
   created () {
-    new Promise((resolve, reject) => {
-      console.log('log: 外部promise-1')
-      resolve()
-    })
-      .then(() => {
-        console.log('log: 外部第一个then-2')
-        new Promise((resolve, reject) => {
-          console.log('log: 内部promise-3')
-          resolve()
-        })
-          .then(() => {
-            console.log('log: 内部第一个then-4')
-            new Promise((resolve, reject) => {
-              resolve()
-            }).then(() => { console.log('log: 内部第二个then-6') })
-          })
-          .then(() => {
-            // console.log('log: 内部第二个then-6')
-          })
-      })
-      .then(() => {
-        console.log('log: 外部第二个then-5')
-      })
 
-    // log: 外部promise
-    // log: 外部第一个then
-    // log: 内部promise
-    // log: 内部第一个then
-    // log: 外部第二个then
-    // log: 内部第二个then
-
-    // let p = new Promise((resolve, reject) => {
-    //   setTimeout(resolve, 1000)
-    // })
-    // p.then(() => {
-    //   console.log('log: 外部第一个then')
-    // })
-    // p.then(() => {
-    //   console.log('log: 外部第二个then')
-    // })
-    // p.then(() => {
-    //   console.log('log: 外部第三个then')
-    // })
   },
   methods: {
     getEleNodeType () {

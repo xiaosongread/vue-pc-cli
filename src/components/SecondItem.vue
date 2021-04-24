7af4e722b8d87df6f0a28c1c24bacef8e8682cd3
songyanbin <598349146@qq.com>
2021-04-24 10:59:16 +0800

    fix

---
+++ src/components/SecondItem.vue
@@ -0,0 +1,105 @@
+<template>
+<div class="listItem">
+	<div class="listdet1">
+		<div class="listdet1-11">
+			<p style="width: 590px; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+				<a href="/article/show-5718.html" target="_blank">100余件文物展示垣曲北白鹅考古成果</a>
+			</p>
+		</div>
+		<div class="buttonxx">
+			<a href="/article/show-5718.html" target="_blank">
+				<img src="../assets/SXWB/img/buttonxx.jpg">
+			</a>
+		</div>
+	</div>
+	<div class="listdet2">近日，山西省考古研究院举办“燕姬的嫁妆——垣曲北白鹅考古揭示的周代女性生活”展览媒体开放日活动。</div>
+	<div class="listdet3">
+		<div class="underline"></div>
+		<div class="underdata">2021年04月22日</div>
+	</div>
+</div>
+</template>
+<script>
+export default {
+  name: 'SecondItem',
+  data () {
+    return {
+      
+    }
+  },
+  components: {
+
+  },
+  created () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.listdet1 {
+    margin-top: 50px;
+    padding-left: 15px;
+    width: 650px;
+    height: 30px;
+    float: left;
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 30px;
+}
+.listdet1-11 {
+    width: 590px;
+    height: 30px;
+    float: left;
+		p{
+			width: 590px;
+			overflow: hidden;
+			white-space: nowrap;
+			text-overflow: ellipsis;
+		}
+}
+.buttonxx {
+    margin-top: 6px;
+    height: 20px;
+    width: 40px;
+    float: right;
+}
+.listdet2 {
+    margin-top: 16px;
+    padding-left: 15px;
+    padding-right: 50px;
+    width: 650px;
+    height: 50px;
+    float: left;
+    font-size: 16px;
+    color: #525252;
+    line-height: 28px;
+    text-indent: 2em;
+}
+.listdet3 {
+    margin-top: 18px;
+    width: 631px;
+    height: 20px;
+    float: left;
+}
+.underline {
+    margin-top: 10px;
+    width: 518px;
+    height: 2px;
+    border: 2px solid #F2F2F2;
+    border-left: 0px;
+    border-right: 0px;
+    border-bottom: 0px;
+    float: left;
+}
+.underdata {
+    height: 20px;
+    width: 108px;
+    float: right;
+    font-size: 14px;
+    color: #5E5E5E;
+}
+</style>
---
+++ src/views/detail/Index.vue
@@ -0,0 +1,127 @@
+<template>
+<div>
+  <IndexLogoHeader/>
+  <Header/>
+  <div class="listmain1">
+    <img src="../../assets/SXWB/img/listad1.jpg" class="headerImg">
+    <div class="listmain1left">
+      <div class="title">借进博“东风”，推进更高水平对外开放</div>
+      <div class="time">
+        <span>时间：2021-09-09</span>
+        <span>作者：小张</span>
+      </div>
+      <div class="cons">文章的内容</div>
+      <div class="subTit">
+        <span>来源：山西日报</span>
+        <span>【责任编辑：lcx】</span>
+      </div>
+      <div class="thirdtext2">
+				<p style="width: 800px; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">【上一篇】<a href="/hall/show-5607.html">以特色演艺助力文旅融合</a>
+				</p>
+				<p style="width: 800px; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+				【下一篇】<a href="/hall/show-5663.html">“品牌行”外延效应持续扩大</a>
+				</p>
+      </div>
+    </div>
+    <SecondNav></SecondNav>
+  </div>
+  <Footer/>
+</div>
+</template>
+
+<script>
+// import { getAllDict, getDouBanMovieList } from '@/utils/api'
+import IndexLogoHeader from '@/components/IndexLogoHeader'
+import Header from '@/components/Header'
+import SecondItem from '@/components/SecondItem'
+import SecondNav from '@/components/SecondNav'
+
+
+
+
+import Footer from '@/components/Footer'
+let that
+export default {
+  components: {
+    IndexLogoHeader,
+    Header,
+    SecondNav,
+    SecondItem,
+
+    Footer
+  },
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    
+  },
+  methods: {
+    // async getDict () {
+    //   const data = await getAllDict()
+    //   if (data.code === 1 && data.data) {
+    //     console.log('接口数据', data)
+    //   }
+    // },
+    // durationNotify () {
+    //   this.$notify.open({
+    //     content: '10秒后自动关闭！',
+    //     duration: 1000
+    //   })
+    // }
+  }
+}
+</script>
+<style scoped>
+.conBox{
+  overflow: hidden;
+}
+.listmain1 {
+    width: 1080px;
+    height: 2040px;
+    margin: 0 auto;
+    margin-top: 5px;
+}
+.headerImg{
+  margin-top: 10px;
+  width: 1080px;
+  height: 114px;
+}
+.listmain1left {
+    width: 691px;
+    float: left;
+    font-size: 14px;
+}
+.title{
+  line-height: 39px;
+  font-size: 28px;
+  font-weight: bold;
+  margin-left: 30px;
+  margin-top: 37px;
+}
+.cons{
+  margin-left: 30px;
+}
+.time,.subTit{
+  height: 40px;
+  margin-left: 30px;
+  margin-top: 35px;
+  border-bottom: 3px solid #EFEFEF;
+}
+.subTit{
+  display: flex;
+  justify-content: space-between;
+}
+.thirdtext2 {
+    padding-left: 45px;
+    height: 14px;
+    width: 691px;
+    float: left;
+    font-size: 16px;
+    line-height: 32px;
+    color: #6E0A0A;
+    margin-bottom: 40px;
+}
+</style>

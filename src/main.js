aa1c6707548dbf511d6ad53494c5d52eb339afb7
OSX <7528794@qq.com>
2021-07-06 16:44:52 +0800

    zz

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 import store from './store'
 import '@/utils/request'
 import VueBlu from 'vue-blu'
 import 'vue-blu/dist/css/vue-blu.min.css'
 import Pagination from 'vue-pagination-2'
+import App from './App'
+import router from './router'
 Vue.component('pagination', Pagination)
 
 // import { Message } from 'element-ui'

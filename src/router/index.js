64d0e09c96867aa23db01d0f9a754b6b616f484c
HUAWEIsyb <598349146@qq.com>
2024-04-12 00:48:30 +0800

    党建PC

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,9 @@ import Layout from '../views/layout/Layout'
 Vue.use(Router)
 
 export default new Router({
-  scrollBehavior(to, from, saveTop) {
+  scrollBehavior (to, from, saveTop) {
     if (saveTop) {
-      return saveTop;
+      return saveTop
     } else {
       return { x: 0, y: 0 }
     }
@@ -24,72 +24,6 @@ export default new Router({
           name: 'Index',
           component: () => import('@/views/index/Index'),
           meta: { title: '首页' }
-        },{
-          path: 'search',
-          name: 'Search',
-          component: () => import('@/views/search/Index'),
-          meta: { title: '全局搜索' }
-        },
-        {
-          path: 'article',
-          name: 'Article',
-          component: () => import('@/views/article/Index'),
-          meta: { title: '文产时讯' }
-        },
-        {
-          path: 'photos',
-          name: 'Photos',
-          component: () => import('@/views/photos/Index'),
-          meta: { title: '美丽山西' }
-        },
-        {
-          path: 'hall',
-          name: 'Hall',
-          component: () => import('@/views/hall/Index'),
-          meta: { title: '文化会馆' }
-        },
-        {
-          path: 'dock',
-          name: 'Dock',
-          component: () => import('@/views/dock/Index'),
-          meta: { title: '产业对接' }
-        },
-        {
-          path: 'wenbo',
-          name: 'Wenbo',
-          component: () => import('@/views/wenbo/Index'),
-          meta: { title: '网上文博' }
-        },
-        {
-          path: 'bbs',
-          name: 'bbs',
-          component: () => import('@/views/bbs/Index'),
-          meta: { title: '文化论坛' }
-        },
-        {
-          path: 'secondIndex',
-          name: 'SecondIndex',
-          component: () => import('@/views/secondIndex/Index'),
-          meta: { title: '二级首页' }
-        },
-        {
-          path: 'photoList',
-          name: 'photoList',
-          component: () => import('@/views/photoList/Index'),
-          meta: { title: '图片列表' }
-        },
-        {
-          // path: 'detail/:id',
-          path: 'detail',
-          name: 'detail',
-          component: () => import('@/views/detail/Index'),
-          meta: { title: '详情页面' },
-        },
-        {
-          path: 'demo',
-          name: 'demo',
-          component: () => import('@/views/demo/Index'),
-          meta: { title: 'demo页面' },
         }
       ]
     }
--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,26 +1,113 @@
 <template>
-<div>
-  <IndexLogoHeader/>
-  <Header :list="menuList" type="index"/>
-  <div class="mbmain">
-    <div class="mb1">
-      <IndexMb1Left :listData="wczxData"/>
-      <NavYwsd :ywsdData="ywsdData" :zcfgData="zcfgData" :ggtzData="ggtzData"/>
-      <img src="@/assets/SXWB/img/mbimg30.jpeg" class="mbimg1">
-      <IndexMb2Left :listMLSXData="mlsxData" :listWHHGData="whhgData" v-if="mlsxData"/>
-      <IndexMb2Right :mlsxData="mlsxData"/>
-      <img src="@/assets/SXWB/img/mbimg30.jpeg" class="mbimg1">
-      <IndexMb3Left :listCYDJData="cydjData" :listWSWBData="wswbData" :listWHLTData="whltData"/>
-      <IndexMb3Right :dxalData="dxalData"/>
-      <img src="" class="mg3">
+<div class="body">
+  <div class="header">
+    <span class="title">人民数科党建信息平台</span>
+  </div>
+  <div class="app-main appMainIndex">
+    <div class="search-header">
+      <p class="search-header-title">人民数科党建信息平台</p>
+      <div class="search-header-box">
+        <div class="search-header-box-input">
+          搜索内容
+        </div>
+      </div>
     </div>
-  </div>  
-  <Footer/>
+    <div class="section-box">
+      <div class="sw-lists-button">
+        <div class="sw-lists-button__item">
+          <img src="../../assets/dj/zxjh.png" class="sw-lists-button__item--icon">
+          <span class="sw-lists-button__item--label">总书记重要讲话</span>
+        </div>
+        <div class="sw-lists-button__item">
+          <img src="../../assets/dj/rmrb.png" class="sw-lists-button__item--icon">
+          <span class="sw-lists-button__item--label">人民日报</span>
+        </div>
+        <div class="sw-lists-button__item">
+          <img src="../../assets/dj/mszk.png" class="sw-lists-button__item--icon">
+          <span class="sw-lists-button__item--label">民生周刊</span>
+        </div>
+      </div>
+      <div class="mid_title">
+        <img  src="../../assets/dj/index_title_l.svg" alt="">
+        <p class="mid_title_text">总书记重要讲话</p>
+        <img src="../../assets/dj/index_title_r.svg" alt="">
+      </div>
+      <div class="el-tabs__content">
+        <div class="multi_list">
+          <div class="title">
+            <span>人民日报社论汇编</span>
+            <span class="refresh">
+              <i class="el-icon-refresh"></i>
+              换一批
+            </span>
+          </div>
+          <div class="content">
+            <div class="sw-lists">
+              <div class="sw-lists__item is-icon is-dot">
+                <div class="sw-lists__item--label">
+                  <div class="sw-lists__item--text">踔厉奋发新征程勇毅前行向复兴</div>
+                </div>
+              </div>
+              <div class="sw-lists__item is-icon is-dot">
+                <div class="sw-lists__item--label">
+                  <div class="sw-lists__item--text">踔厉奋发新征程勇毅前行向复兴</div>
+                </div>
+              </div>
+              <div class="sw-lists__item is-icon is-dot">
+                <div class="sw-lists__item--label">
+                  <div class="sw-lists__item--text">踔厉奋发新征程勇毅前行向复兴</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="multi_list">
+          <div class="title">
+            <span>人民日报社论汇编</span>
+            <span class="refresh">
+              <i class="el-icon-refresh"></i>
+              换一批
+            </span>
+          </div>
+          <div class="content">
+            <div class="sw-lists">
+              <div class="sw-lists__item is-icon is-dot">
+                <div class="sw-lists__item--label">
+                  <div class="sw-lists__item--text">踔厉奋发新征程勇毅前行向复兴</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="multi_list">
+          <div class="title">
+            <span>人民日报社论汇编</span>
+            <span class="refresh">
+              <i class="el-icon-refresh"></i>
+              换一批
+            </span>
+          </div>
+          <div class="content">
+            <div class="sw-lists">
+              <div class="sw-lists__item is-icon is-dot">
+                <div class="sw-lists__item--label">
+                  <div class="sw-lists__item--text">踔厉奋发新征程勇毅前行向复兴</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="copyright_cont">
+    <p>民生周刊版权所有 未经书面授权禁止使用</p>
+  </div>
 </div>
 </template>
 
 <script>
-import { dataToJsonArticle, menuDataList,topDataListJson } from '@/utils/api'
+import { dataToJsonArticle, menuDataList, topDataListJson } from '@/utils/api'
 import IndexLogoHeader from '@/components/IndexLogoHeader'
 import Header from '@/components/Header'
 import IndexMb1Left from '@/components/IndexMb1Left'
@@ -45,108 +132,244 @@ export default {
   },
   data () {
     return {
-      menuList: [],
-      data: [],
-      whltData: [],
-      wswbData: [],
-      cydjData: [],
-      whhgData: [],
-      mlsxData: [],
-      wczxData: [],
-      ywsdData: [],
-      zcfgData: [],
-      ggtzData: [],
-      dxalData: [],
+
     }
   },
   created () {
     this.menuDataList()
-    this.zcfgdataToJsonArticle()
-    this.ywsddataToJsonArticle()
-    this.ggtzdataToJsonArticle()
-    this.dxaldataToJsonArticle()
-    this.whltDataListJson()
-    this.wswbDataListJson()
-    this.cydjDataListJson()
-    this.whhgDataListJson()
-    this.mlsxDataListJson()
-    this.wczxDataListJson()
     _that = this
-
   },
 
   methods: {
-    async menuDataList() {
+    async menuDataList () {
       const data = await menuDataList()
       this.menuList = data.data
-    },
-    async whltDataListJson() {
-      const data = await topDataListJson({channelId:20})
-      this.whltData = data.data[0].data
-    },
-    async wswbDataListJson() {
-      const data = await topDataListJson({channelId:19})
-      this.wswbData = data.data[0].data
-    },
-    async cydjDataListJson() {
-      const data = await topDataListJson({channelId:18})
-      this.cydjData = data.data[0].data
-    },
-    async whhgDataListJson() {
-      const data = await topDataListJson({channelId:17})
-      this.whhgData = data.data[0].data
-    },
-    async mlsxDataListJson() {
-      const data = await topDataListJson({channelId:15})
-      this.mlsxData = data.data[0].data
-    },
-    async wczxDataListJson() {
-      const data = await topDataListJson({channelId:13})
-      this.wczxData = data.data[0].data
-    },
-    async ywsddataToJsonArticle () {
-      const data = await dataToJsonArticle({categoryId:59})
-      _that.ywsdData=data
-    },
-    async zcfgdataToJsonArticle () {
-      const data = await dataToJsonArticle({categoryId:62})
-      _that.zcfgData=data
-    },
-    
-    async ggtzdataToJsonArticle () {
-      const data = await dataToJsonArticle({categoryId:63})
-      _that.ggtzData=data
-    },
-    async dxaldataToJsonArticle () {
-      const data = await dataToJsonArticle({categoryId:86})
-      _that.dxalData=data
     }
   }
 }
 </script>
 <style rel="stylesheet/scss" lang="scss" scoped>
-.mbmain {
-  margin-top: 25px;
-  width: 1080px;
-  margin: 0px auto;
+.body{
+  background: #f5f5f5;
 }
-.mb1 {
-  margin-top: 30px;
-  width: 1080px;
+.header {
+  width: 100%;
+    height: 56px;
+    max-width: 100%;
+    background-color: #D42A2A;
+    color: #fff;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 1116;
+    font-size: 16px;
+    padding: 0 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: hsla(0, 0%, 100%, .2);
+    .title {
+      color: rgb(255, 218, 128);
+      font-weight: 600;
+      font-family: 微软雅黑;
+      font-size: 18px;
+    }
 }
-.mbimg1 {
-  margin-top: 60px;
-  margin-bottom: 45px;
-  height: 162px;
-  width: 1080px;
+.appMainIndex {
+  min-height: calc(100vh - 60px);
+  background: url(../../assets/dj/index_bg.png) no-repeat top / 100% 460px;
+  width: 100%;
+  overflow: auto;
+  padding-top: 56px;
+  .search-header {
+    text-align: center;
+    padding: 100px 0 50px;
+    .search-header-title {
+      color: rgb(255, 227, 148);
+      font-weight: 600;
+      font-family: "Heiti SC";
+      font-size: 48px;
+      letter-spacing: 5px;
+    }
+    .search-header-box {
+      display: flex;
+      justify-content: center;
+      margin: 40px 0 6px;
+      .search-header-box-input {
+        display: flex;
+        background-color: #fff;
+        border-radius: 8px;
+        border: 2px solid #cacaca;
+        width: 900px;
+        height: 52px;
+        box-sizing: border-box;
+        text-align: left;
+      }
+    }
+  }
+  .section-box {
+    width: 1200px;
+    margin: 0 auto;
+    padding-bottom: 40px;
+    .sw-lists-button {
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      gap: 8px;
+      -webkit-user-select: none;
+      margin: 0 auto;
+      .sw-lists-button__item {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        // flex: 1;
+        width: 160px;
+        height: 150px;
+        border-radius: 8px;
+        background: #fff;
+        border: 1px solid transparent;
+        cursor: pointer;
+        transition: all 0.3s;
+        &:hover {
+          border-color: #d42a2a;
+          background: #fff;
+          box-shadow: 0px 4px 24px 0px #f6d4d4;
+        }
+        .sw-lists-button__item--icon {
+          width: 70px;
+          height: 70px;
+          margin-bottom: 14px;
+        }
+        .sw-lists-button__item--label {
+          color: #5c5c5c;
+          text-align: center;
+          font-feature-settings: "clig" off, "liga" off;
+          font-family: Source Han Sans CN;
+          font-size: 16px;
+          font-style: normal;
+          font-weight: 500;
+          line-height: 24px;
+        }
+      }
+    }
+    .mid_title {
+      width: 100%;
+      margin: 48px 0 46px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 20px;
+      .mid_title_text {
+        text-align: center;
+        width: auto;
+        height: 52px;
+        line-height: 52px;
+        font-size: 32px;
+        background: linear-gradient(90deg, #f72b23, #ff5a13);
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-family: Microsoft YaHei;
+        font-weight: 700;
+        word-wrap: break-word;
+        cursor: pointer;
+      }
+    }
+    .el-tabs__content {
+      overflow: inherit;
+      min-height: 480px;
+      position: relative;
+      display: flex;
+      gap: 18px;
+      .multi_list {
+        flex: 1;
+        height: 480px;
+        border-radius: 8px;
+        overflow: hidden;
+        background-color: #fff;
+        &:hover {
+          box-shadow: 0 2px 24px rgba(0,0,0,.16)
+        }
+        .title {
+          width: 100%;
+          height: 80px;
+          background: url(../../assets/dj/index_djan_bg.png) no-repeat top / 100% 100%;
+          color: #ffea9e;
+          font-size: 20px;
+          font-weight: 700;
+          word-wrap: break-word;
+          text-align: left;
+          padding: 26px 20px;
+          margin-bottom: 0px;
+          display: flex;
+          justify-content: space-between;
+          align-items:self-end;
+          .refresh {
+            font-size: 14px;
+            cursor: pointer;
+          }
+        }
+        .content {
+          padding: 10px;
+          .sw-lists__item.is-icon {
+            padding-left: 20px;
+          }
+          .sw-lists__item {
+            position: relative;
+            transition: background-color 0.3s;
+          }
+          .sw-lists__item.is-dot::before {
+            content: "";
+            position: absolute;
+            width: 6px;
+            height: 6px;
+            border-radius: 50%;
+            background-color: #3e3e3e;
+            left: 10px;
+            top: 20px;
+            transform: translate(-50%, -50%);
+          }
+          .sw-lists__item--label {
+            display: flex;
+            justify-content: space-between;
+            color: #3e3e3e;
+            font-family: Source Han Sans CN;
+            font-size: 16px;
+            line-height: 24px;
+            font-style: normal;
+            font-weight: 500;
+            transition: color 0.3s;
+            cursor: pointer;
+            padding: 8px;
+            .sw-lists__item--text {
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+            }
+            &:hover {
+              color: #d42a2a;
+            }
+          }
+        }
+      }
+    }
+  }
 }
-.mb2 {
-    height: 1245px;
-    width: 1080px;
-}
-.mg3{
-  margin-top: 35px;
-  height: 0px;
-  width: 1080px;
+.copyright_cont {
+  width: 100%;
+  height: 60px;
+  background-color: #d42a2a;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p{
+    color: #fff;
+    font-size: 16px;
+    letter-spacing: .5px;
+    word-wrap: break-word;
+  }
 }
 </style>
